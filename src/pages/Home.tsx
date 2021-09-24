74d9ef52406a3fd009589456e7ed88454c76b733
NearMaick <maick_a_s@msn.com>
2021-09-24 13:01:18 -0300

    refactor(web): NLW01 AuthContext

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { useHistory } from 'react-router';
 
-import { AuthContext } from '../App';
+import { AuthContext } from '../contexts/AuthContext';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
